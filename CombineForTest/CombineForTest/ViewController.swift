937f438639ccafdd040fcea4ac90d983faa0d3cb
Vadim Volyas <VVolyas@alfabank.ru>
2024-10-24 17:25:06 +0300

    lesson 22: publusher

--- CombineForTest/CombineForTest/ViewController.swift
+++ CombineForTest/CombineForTest/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemMint
+        
+        // Publisher
+        
+        let publisher = Just("Hello world!")
+        
+        let arryaPublisher = ["1", "2", "3"].publisher
+        
+        // Subscribe
+        
+        publisher.sink { _ in
+            print("Value recieved")
+            // срабатывает после того как вы получили новое значение
+        } receiveValue: { value in
+            print("This value: \(value)")
+        }
+        
+        arryaPublisher.sink { value in
+            print("Array value: \(value)")
+        }
     }
-
+    
 
 }
 
